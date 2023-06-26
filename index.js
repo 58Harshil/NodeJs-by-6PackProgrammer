e740ceb44abaaf322e4601f7deb283f60c3b378e
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-06-26 18:22:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+/*import http from "http"
+import {generateLovePercent} from "./features.js"
+import fs from "fs"
+import path from "path" 
+//console.log(path.extname("/home/random/inde.js"));
+//import { log } from "console"
+
+ /*const home = fs.readFile("./index.html", function(){
+    console.log("File read");
+ })*/
+ /*const home = fs.readFileSync("./index.html")
+ console.log(home);*/
+ //console.log(home);
+//console.log(generateLovePercent())*/
+ /*const server = http.createServer(function(req,res){
+    console.log(req.method);
+   if(req.url==="/about"){
+    res.end(`<h1>Love is ${generateLovePercent()}</h1>`)
+   }
+   else if(req.url==="/"){
+    res.end("home")
+   }
+   else if(req.url==="/contact"){
+    res.end("<h1>Contact Page</h1>")
+   }
+   else{
+    res.end("<h1>Page not found</h1>")
+   }
+    
+ });
+ server.listen(5000,function(){
+    console.log("Sever is working");
+ })*/
